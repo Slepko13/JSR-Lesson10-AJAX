d51948b04890675a7342c03dcd69ebe4d5c97c92
Mykola Slepko <slepko13@gmail.com>
2020-05-07 16:17:16 +0300

    search list item onclick added

--- main.js
+++ main.js
@@ -80,9 +80,7 @@ function startSearch() {
                 searchList.innerHTML = `<div>No such value "${searchValue}" in "${prop}" category</div>`;
                 searchPreloader.style.display = "none";
                 searchButton.after(searchList);
-
             }
-
             if (data.results.length === 1) {
                 let obj = data.results[0];
                 console.log('obj: ', obj);
@@ -132,18 +130,13 @@ function startSearch() {
                                             subList.append(subListItem);
                                         }
 
-
                                     });
-
-
                             };
                             searchList.append(subList);
                         } else {
-
                             let listElem = document.createElement('li');
                             listElem.innerHTML = `${key} : <span>${obj[key]}</span>;`
                             searchList.append(listElem);
-
                         }
                 }
                 searchPreloader.style.display = "none";
@@ -151,7 +144,7 @@ function startSearch() {
 
             } else {
                 let arr = data.results;
-                arr.map(item => searchList.innerHTML += `<li>${item.name}</li>`);
+                arr.map(item => searchList.innerHTML += `<li class="searchListItem" onclick="chooseItem(this.innerText,document.getElementById('propTypeSearch').value)"> ${item.name}</li>`);
                 searchPreloader.style.display = "none";
                 searchButton.after(searchList);
             }
@@ -160,21 +153,95 @@ function startSearch() {
 
 }
 
-async function films(title, year) {
-    // const title = "A New Hope";
-    // const year = 1977
-    let poster = document.createElement('img');
-    const url = `http://www.omdbapi.com/?s=${title}&y=${year}&apikey=c19821a5`;
-    await fetch(url)
-        .then(response => response.json())
+function chooseItem(value, prop) {
+    if (searchList) {
+        searchList.remove();
+    }
+    searchList = document.createElement('ul');
+    searchPreloader.style.display = "block";
+    let searchButton = document.getElementById('searchButton');
+    // let prop = document.getElementById('propTypeSearch').value;
+    let searchValue = document.getElementById('searchInput').value;
+    if (!searchValue) {
+        searchList.innerHTML = `<div>Input value required.</div>`;
+        searchPreloader.style.display = "none";
+        searchButton.after(searchList);
+    }
+    fetch(mainUrl + `${prop}/?search=${value}`).then(response => response.json())
         .then(data => {
-            console.log(data);
-            console.log(data.Search[0].Poster);
-            // poster.setAttribute('src', data.Search[0].Poster);
-            return data.Search[0].Poster;
+            if (data.count === 0) {
+                searchList.innerHTML = `<div>No such value "${searchValue}" in "${prop}" category</div>`;
+                searchPreloader.style.display = "none";
+                searchButton.after(searchList);
+            }
+            if (data.results.length === 1) {
+                let obj = data.results[0];
+                console.log('obj: ', obj);
+                for (key in obj) {
+                    if (key === "homeworld") {
+                        let home = key;
+                        console.log('key before fetch: ', key);
+                        console.log('obj[key]: ', obj[key]);
+                        /*Чому я не можу в 96 рядку замісь ${home} прописати ${key}, чому значення key з homeworld  змінюється на url?*/
+                        fetch(obj[key]).then(response => response.json())
+                            .then(data => {
+                                console.log('fetch data :', data);
+                                console.log('key in fetch: ', key);
+                                console.log('fetch data[key]: ', data[key]);
+                                let listElem = document.createElement('li');
+                                listElem.innerHTML = `${home} : <span>${data.name}</span>;`
+                                searchList.append(listElem);
+                            });
+                    }
+                    else
+                        if (typeof (obj[key]) == "object") {
+                            let subList = document.createElement('ol');
+                            subList.innerHTML = `${key}`;
+                            for (let i = 0; i < obj[key].length; i++) {
+                                let subListItem = document.createElement('li');
+                                fetch(obj[key][i]).then(response => response.json())
+                                    .then(data => {
+                                        if (data.title) {
+                                            subList.classList.add('films');
+                                            console.log(data.title);
+                                            console.log(parseInt(data["release_date"]));
+                                            fetch(`http://www.omdbapi.com/?s=${data.title}&y=${parseInt(data["release_date"])}&apikey=c19821a5`)
+                                                .then(response => response.json())
+                                                .then(data => {
+                                                    console.log(data);
+                                                    console.log(data.Search[0].Poster);
 
+                                                    subListItem.innerHTML = `<p></p><div>${data.Search[0].Title}</div><p></p><img src="${data.Search[0].Poster}">`;
+                                                    subList.append(subListItem);
+
+                                                })
+
+
+                                        } else {
+
+                                            subListItem.innerHTML = `<span>${data.name}</span>`;
+                                            subList.append(subListItem);
+                                        }
+
+                                    });
+                            };
+                            searchList.append(subList);
+                        } else {
+                            let listElem = document.createElement('li');
+                            listElem.innerHTML = `${key} : <span>${obj[key]}</span>;`
+                            searchList.append(listElem);
+                        }
+                }
+                searchPreloader.style.display = "none";
+                searchButton.after(searchList);
+
+            } else {
+                let arr = data.results;
+                arr.map(item => searchList.innerHTML += `<li  ${item.name}</li>`);
+                searchPreloader.style.display = "none";
+                searchButton.after(searchList);
+            }
         })
-    // poster.setAttribute('src', '')
-    // subListItem.append(poster);
+
+
 }
-// films("A New Hope", 1977);
